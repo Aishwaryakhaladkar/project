010cd25511a27e25a5c18f3e0d00a071162ae410
Aishwarya <aishwaryakhaladkar1@gmail.com>
2022-01-17 01:08:09 +0530

    First Commit
    uploading files

---
+++ Ais/src/Abc/ConstructorWithTypes.java
@@ -0,0 +1,54 @@
+package Abc;
+import java.util.*;
+
+class Demo{
+	Demo(){
+		System.out.println("zero or no argument constructor.....");
+	}
+}
+	
+ class AdharCard {
+		String name;
+		String dob;
+		long contactno;
+	    
+		
+		AdharCard(String personname,String persondob,long cno){
+			name=personname;
+			dob=persondob;
+			contactno=cno;
+		}
+
+	}
+	
+
+
+
+
+
+
+
+
+
+public class ConstructorWithTypes {
+	public static void main(String[] args) {
+	      
+		new Demo();
+		
+  //parameterized constructor defined
+		AdharCard a1=new AdharCard("Aishwarya","1235",123456789);
+		System.out.println("name= "+a1.name);
+		System.out.println("dob= "+a1.dob);
+		System.out.println("contactno= "+a1.contactno);
+		
+		System.out.println("-----------------");
+		AdharCard a2=new AdharCard("Prisha","3456",4567834);
+		System.out.println("name= "+a2.name);
+		System.out.println("dob= "+a2.dob);
+		System.out.println("contactno= "+a2.contactno);
+	}
+
+}
+
+
+	
